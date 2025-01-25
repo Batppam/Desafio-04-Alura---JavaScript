02189bd67b228a20e8edbf952dde1d732357bb25
Batppam <107057754+Batppam@users.noreply.github.com>
2025-01-25 16:14:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ titulo.innerHTML = 'Hora do desafio';
 //Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
 
 function consoleClick(){
-    consoleClick.log('O botão foi clicado');
+    console.log('O botão foi clicado');
 }
 
 //Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
@@ -18,7 +18,7 @@ function alertaClick(){
 //Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
 function promptClick(){
     let cidade = prompt('Qual sua cidade?');
-    promptClick.log(`Estive em ${cidade} e lembrei de você`);
+    console.log(`Estive em ${cidade} e lembrei de você`);
 }
 
 //Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
